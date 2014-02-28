dd124438a627b6d59c0124acd8534e7902720432
Joslyn <jrosbrook@gmail.com>
2014-02-27 16:11:16 -0800

    added string concatenation

--- lib/binary_tree.rb
+++ lib/binary_tree.rb
@@ -9,7 +9,7 @@ class Tree
 
   def self.preorder(tree)
     return if tree.nil?
-    print tree.name.to_s + ""
+    print tree.name.to_s + " "
     preorder(tree.left)
     preorder(tree.right)
   end 
@@ -17,7 +17,7 @@ class Tree
   def self.inorder(tree)
     return if tree.nil?
     inorder(tree.left)
-    print tree.name.to_s + ""
+    print tree.name.to_s + " "
     inorder(tree.right)
   end 
 
@@ -25,6 +25,6 @@ class Tree
     return if tree.nil?
     postorder(tree.left)
     postorder(tree.right)
-    print tree.name.to_s + ""
+    print tree.name.to_s + " "
   end 
 end
